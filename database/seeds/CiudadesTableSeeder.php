1d0d6996e957c4b5945859307b06752f97a7226d
gemina.lopez.ext@boehringer-ingelheim.com <gemina.lopez.ext@boehringer-ingelheim.com>
2019-11-05 14:38:32 -0300

    ciudades y provincias

--- database/seeds/CiudadesTableSeeder.php
+++ database/seeds/CiudadesTableSeeder.php
@@ -11,6 +11,198 @@ class CiudadesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Cargo datos
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 1,
+            'ciudad' => 'Morón',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 3,
+            'ciudad' => 'San Justo',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 4,
+            'ciudad' => 'Ciudad Autónoma de Buenos Aires',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 5,
+            'ciudad' => 'La Plata',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 6,
+            'ciudad' => 'Luján',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 7,
+            'ciudad' => 'San Martín',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 8,
+            'ciudad' => 'Bahía Blanca',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 9,
+            'ciudad' => 'Tigre',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 10,
+            'ciudad' => 'Ezeiza',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 11,
+            'ciudad' => 'Caseros',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 12,
+            'ciudad' => 'Merlo',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 13,
+            'ciudad' => 'Lomas de Zamora',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 14,
+            'ciudad' => 'San Pedro',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 15,
+            'ciudad' => 'Moreno',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 16,
+            'ciudad' => 'Merlo',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 17,
+            'ciudad' => 'San Antonio de Padua',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 18,
+            'ciudad' => 'Ituzaingó',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 19,
+            'ciudad' => 'San Miguel del Monte',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 20,
+            'ciudad' => 'Azul',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 21,
+            'ciudad' => 'Junín',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 22,
+            'ciudad' => 'Olavarría',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 23,
+            'ciudad' => 'Campana',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 24,
+            'ciudad' => 'Necochea',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 25,
+            'ciudad' => 'Pergamino',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 26,
+            'ciudad' => 'Tandil',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 27,
+            'ciudad' => 'Banfield',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 28,
+            'ciudad' => 'Temperley',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 29,
+            'ciudad' => 'Berazategui',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 30,
+            'ciudad' => 'Cañuelas',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 31,
+            'ciudad' => 'Zárate',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 32,
+            'ciudad' => 'Wilde',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('ciudades')->insert([
+            'id_ciudad' => 33,
+            'ciudad' => 'San Isidro',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
     }
 }
--- database/seeds/ProvinciasTableSeeder.php
+++ database/seeds/ProvinciasTableSeeder.php
@@ -11,6 +11,150 @@ class ProvinciasTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+         // Cargo datos
+         DB::table('provincias')->insert([
+            'id_provincia' => 1,
+            'provincia' => 'Misiones',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 2,
+            'provincia' => 'San Luis',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 3,
+            'provincia' => 'San Juan',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 4,
+            'provincia' => 'Entre Ríos',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 5,
+            'provincia' => 'Santa Cruz',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 6,
+            'provincia' => 'Río Negro',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 7,
+            'provincia' => 'Chubut',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 8,
+            'provincia' => 'Córdoba',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 9,
+            'provincia' => 'Mendoza',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 10,
+            'provincia' => 'La Rioja',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 11,
+            'provincia' => 'Catamarca',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 12,
+            'provincia' => 'La Pampa',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 13,
+            'provincia' => 'Santiago del Estero',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 14,
+            'provincia' => 'Corrientes',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 15,
+            'provincia' => 'Santa Fe',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 16,
+            'provincia' => 'Tucumán',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 17,
+            'provincia' => 'Neuquén',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 18,
+            'provincia' => 'Salta',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 19,
+            'provincia' => 'Chaco',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 20,
+            'provincia' => 'Formosa',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 21,
+            'provincia' => 'Jujuy',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 22,
+            'provincia' => 'Ciudad Autónoma de Buenos Aires',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 23,
+            'provincia' => 'Buenos Aires',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('provincias')->insert([
+            'id_provincia' => 24,
+            'provincia' => 'Tierra del Fuego, Antártida e Islas del Atlántico Sur',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
     }
 }
